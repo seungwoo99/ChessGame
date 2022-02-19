3ee2b5a1ba114d9918943c1492372bc03ddd8a08
seungwoo <seung.code@gmail.com>
2022-02-20 00:41:25 +0900

    fifth commit - chess game add calculating rook movement method

--- src/Piece.java
+++ src/Piece.java
@@ -4,17 +4,34 @@ public class Piece {
     //fields
     private String playerName;
     private String PieceName;
-    private int Piece_X;
-    private int Piece_Y;
+    private String empty;
+    private int somethingbetween;
 
-    private String calculatedCo;
-
-    //constructor
-    public Piece(String givenPlayerName, String givenPieceName, int x_cor, int y_cor) {
+    //constructors
+    public Piece(String givenPlayerName, String givenPieceName) {
         playerName = givenPlayerName;
         PieceName = givenPieceName;
-        Piece_X = x_cor;
-        Piece_Y = y_cor;
+        empty = "NotEmpty";
+    }
+    public Piece(int x_cor, int y_cor){
+        empty="empty";
+        if(y_cor == 0){
+            PieceName = "("+(8-x_cor)+","+"A)";
+        }else if(y_cor==1){
+            PieceName = "("+(8-x_cor)+","+"B)";
+        }else if(y_cor==2){
+            PieceName = "("+(8-x_cor)+","+"C)";
+        }else if(y_cor==3){
+            PieceName = "("+(8-x_cor)+","+"D)";
+        }else if(y_cor==4){
+            PieceName = "("+(8-x_cor)+","+"E)";
+        }else if(y_cor==5){
+            PieceName = "("+(8-x_cor)+","+"F)";
+        }else if(y_cor==6){
+            PieceName = "("+(8-x_cor)+","+"G)";
+        }else{
+            PieceName = "("+(8-x_cor)+","+"H)";
+        }
     }
 
     //get methods
@@ -24,29 +41,93 @@ public class Piece {
     public String getPieceName(){
         return PieceName;
     }
-    public int getPawn_X(){
-        return Piece_X;
-    }
-    public int getPawn_Y(){
-        return Piece_Y;
+    public String getEmpty(){
+        return empty;
     }
 
     //calculate where can it move
     //method that calculates pawn moves
-    public String[][] calPawnA(String[][] board, int current_x, int current_y, int wanted_x, int wanted_y){
+    public Piece[][] calPawnA(Piece[][] board, int current_x, int current_y, int wanted_x, int wanted_y){
         boolean check1= (wanted_x<7 && current_x+1 == wanted_x);
         boolean check2=(current_y == wanted_y || current_y+1 == wanted_y || current_y-1 == wanted_y);
         boolean check3=(current_y+1<8 && current_y-1>=0);
 
         if(check1&&check2){
             if(wanted_y<8 && wanted_y >=0) {
-                board[wanted_x][wanted_y] = getPieceName();
-                board[current_x][current_y] = "(" + current_x + "," + current_y + ")";
+                board[wanted_x][wanted_y] = board[current_x][current_y];
+                board[current_x][current_y] = new Piece(current_x, current_y);
                 return board;
             }
             return board;
         }else {
             return board;
         }
+    }
+    //method that calculates rook moves
+    public Piece[][] calRookA(Piece[][] board, int current_x, int current_y, int wanted_x, int wanted_y){
+        boolean check1 = (current_y == wanted_y)&&(wanted_x>=0 && wanted_x<8)&&(current_x!=wanted_x);
+        boolean check2 = (current_x == wanted_x)&&(wanted_y>=0 && wanted_y<8)&&(current_y!=wanted_y);
+        somethingbetween=0;
+        if (check1||check2){
+            //check if other piece is located between them
+            if(current_x<wanted_x){
+                for(int i=current_x+1; i<wanted_x; i++){
+                    if(board[i][current_y].getEmpty().equals("NotEmpty")){
+                        somethingbetween+=1;
+                    }
+                }
+                if (somethingbetween>0){
+                    return board;
+                }else if(somethingbetween==0 && ((board[wanted_x][wanted_y].getEmpty().equals("empty"))||
+                        !board[wanted_x][wanted_y].getPlayerName().equals(board[current_x][current_y].getPlayerName()))){
+                    board[wanted_x][wanted_y] = board[current_x][current_y];
+                    board[current_x][current_y] = new Piece(current_x, current_y);
+                    return board;
+                }
+            }else if(current_x>wanted_x){
+                for(int i=wanted_x+1; i<current_x; i++){
+                    if(board[i][current_y].getEmpty().equals("NotEmpty")){
+                        somethingbetween+=1;
+                    }
+                }
+                if (somethingbetween>0){
+                    return board;
+                }else if(somethingbetween==0 && ((board[wanted_x][wanted_y].getEmpty().equals("empty"))||
+                        !board[wanted_x][wanted_y].getPlayerName().equals(board[current_x][current_y].getPlayerName()))){
+                    board[wanted_x][wanted_y] = board[current_x][current_y];
+                    board[current_x][current_y] = new Piece(current_x, current_y);
+                    return board;
+                }
+            }else if(current_y<wanted_y){
+                for(int i=current_y+1; i<wanted_y; i++){
+                    if(board[current_x][i].getEmpty().equals("NotEmpty")){
+                        somethingbetween+=1;
+                    }
+                }
+                if (somethingbetween>0){
+                    return board;
+                }else if(somethingbetween==0 && ((board[wanted_x][wanted_y].getEmpty().equals("empty"))||
+                        !board[wanted_x][wanted_y].getPlayerName().equals(board[current_x][current_y].getPlayerName()))){
+                    board[wanted_x][wanted_y] = board[current_x][current_y];
+                    board[current_x][current_y] = new Piece(current_x, current_y);
+                    return board;
+                }
+            }else if(current_y>wanted_y){
+                for(int i=wanted_y+1; i<current_y; i++){
+                    if(board[current_x][i].getEmpty().equals("NotEmpty")){
+                        somethingbetween+=1;
+                    }
+                }
+                if (somethingbetween>0){
+                    return board;
+                }else if(somethingbetween==0 && ((board[wanted_x][wanted_y].getEmpty().equals("empty"))||
+                        !board[wanted_x][wanted_y].getPlayerName().equals(board[current_x][current_y].getPlayerName()))){
+                    board[wanted_x][wanted_y] = board[current_x][current_y];
+                    board[current_x][current_y] = new Piece(current_x, current_y);
+                    return board;
+                }
+            }
+        }
+        return board;
     }
 }
--- src/chessGame.java
+++ src/chessGame.java
@@ -2,6 +2,10 @@
 import java.util.Scanner;
 
 public class chessGame {
+    //fields
+    public static int integerY;
+    public static String AlphabetY;
+
     //main method
     public static void main(String[] args) {
         //ask users' name
@@ -12,114 +16,99 @@ public class chessGame {
         String player2 = input.nextLine();
 
         // set player1's pieces
-        Piece player1_Rook1 = new Piece(player1, player1 + "_Rook1", 0, 0);
-        Piece player1_Knight1 = new Piece(player1, player1 + "_Knight1", 0, 1);
-        Piece player1_Bishop1 = new Piece(player1, player1 + "_Bishop1", 0, 2);
-        Piece player1_Queen = new Piece(player1, player1 + "_Queen", 0, 3);
-        Piece player1_King = new Piece(player1, player1 + "_King", 0, 4);
-        Piece player1_Bishop2 = new Piece(player1, player1 + "_Bishop2", 0, 5);
-        Piece player1_Knight2 = new Piece(player1, player1 + "_Knight2", 0, 6);
-        Piece player1_Rook2 = new Piece(player1, player1 + "_Rook2", 0, 7);
-
-        Piece player1_pawn1 = new Piece(player1, player1 + "_Pawn1", 1, 0);
-        Piece player1_pawn2 = new Piece(player1, player1 + "_Pawn2", 1, 1);
-        Piece player1_pawn3 = new Piece(player1, player1 + "_Pawn3", 1, 2);
-        Piece player1_pawn4 = new Piece(player1, player1 + "_Pawn4", 1, 3);
-        Piece player1_pawn5 = new Piece(player1, player1 + "_Pawn5", 1, 4);
-        Piece player1_pawn6 = new Piece(player1, player1 + "_Pawn6", 1, 5);
-        Piece player1_pawn7 = new Piece(player1, player1 + "_Pawn7", 1, 6);
-        Piece player1_pawn8 = new Piece(player1, player1 + "_Pawn8", 1, 7);
+        Piece player1_Rook = new Piece(player1, player1 + "_Rook");
+        Piece player1_Knight = new Piece(player1, player1 + "_Knight");
+        Piece player1_Bishop = new Piece(player1, player1 + "_Bishop");
+        Piece player1_Queen = new Piece(player1, player1 + "_Queen");
+        Piece player1_King = new Piece(player1, player1 + "_King");
+        Piece player1_Pawn = new Piece(player1, player1 + "_Pawn");
 
         //set player2's pieces
-        Piece player2_Rook1 = new Piece(player2, player2 + "_Rook1", 7, 0);
-        Piece player2_Knight1 = new Piece(player2, player2 + "_Knight1", 7, 1);
-        Piece player2_Bishop1 = new Piece(player2, player2 + "_Bishop1", 7, 2);
-        Piece player2_King = new Piece(player2, player2 + "_King", 7, 3);
-        Piece player2_Queen = new Piece(player2, player2 + "_Queen", 7, 4);
-        Piece player2_Bishop2 = new Piece(player2, player2 + "_Bishop2", 7, 5);
-        Piece player2_Knight2 = new Piece(player2, player2 + "_Knight2", 7, 6);
-        Piece player2_Rook2 = new Piece(player2, player2 + "_Rook2", 7, 7);
-
-        Piece player2_pawn1 = new Piece(player2, player2 + "_Pawn1", 6, 0);
-        Piece player2_pawn2 = new Piece(player2, player2 + "_Pawn2", 6, 1);
-        Piece player2_pawn3 = new Piece(player2, player2 + "_Pawn3", 6, 2);
-        Piece player2_pawn4 = new Piece(player2, player2 + "_Pawn4", 6, 3);
-        Piece player2_pawn5 = new Piece(player2, player2 + "_Pawn5", 6, 4);
-        Piece player2_pawn6 = new Piece(player2, player2 + "_Pawn6", 6, 5);
-        Piece player2_pawn7 = new Piece(player2, player2 + "_Pawn7", 6, 6);
-        Piece player2_pawn8 = new Piece(player2, player2 + "_Pawn8", 6, 7);
+        Piece player2_Rook = new Piece(player2, player2 + "_Rook");
+        Piece player2_Knight = new Piece(player2, player2 + "_Knight");
+        Piece player2_Bishop = new Piece(player2, player2 + "_Bishop");
+        Piece player2_Queen = new Piece(player2, player2 + "_Queen");
+        Piece player2_King = new Piece(player2, player2 + "_King");
+        Piece player2_Pawn = new Piece(player2, player2 + "_Pawn");
 
         //set on the board
-        String[][] gameboard = new String[8][8];
-        gameboard[0][0] = player1_Rook1.getPieceName();
-        gameboard[1][0] = player1_pawn1.getPieceName();
-        gameboard[0][1] = player1_Knight1.getPieceName();
-        gameboard[1][1] = player1_pawn2.getPieceName();
-        gameboard[0][2] = player1_Bishop1.getPieceName();
-        gameboard[1][2] = player1_pawn3.getPieceName();
-        gameboard[0][3] = player1_Queen.getPieceName();
-        gameboard[1][3] = player1_pawn4.getPieceName();
-        gameboard[0][4] = player1_King.getPieceName();
-        gameboard[1][4] = player1_pawn5.getPieceName();
-        gameboard[0][5] = player1_Bishop2.getPieceName();
-        gameboard[1][5] = player1_pawn6.getPieceName();
-        gameboard[0][6] = player1_Knight2.getPieceName();
-        gameboard[1][6] = player1_pawn7.getPieceName();
-        gameboard[0][7] = player1_Rook2.getPieceName();
-        gameboard[1][7] = player1_pawn8.getPieceName();
+        Piece[][] gameboard = new Piece[8][8];
+        gameboard[0][0] = gameboard[0][7] = player1_Rook;
+        gameboard[0][1] = gameboard[0][6] = player1_Knight;
+        gameboard[0][2] = gameboard[0][5] = player1_Bishop;
+        gameboard[0][3] = player1_Queen;
+        gameboard[0][4] = player1_King;
+        for (int i = 0; i < 8; i++) {
+            gameboard[1][i] = player1_Pawn;
+            gameboard[6][i] = player2_Pawn;
+        }
+        gameboard[7][0] = gameboard[7][7] = player2_Rook;
+        gameboard[7][1] = gameboard[7][6] = player2_Knight;
+        gameboard[7][2] = gameboard[7][5] = player2_Bishop;
+        gameboard[7][3] = player2_Queen;
+        gameboard[7][4] = player2_King;
 
-        gameboard[7][0] = player2_Rook1.getPieceName();
-        gameboard[6][0] = player2_pawn1.getPieceName();
-        gameboard[7][1] = player2_Knight1.getPieceName();
-        gameboard[6][1] = player2_pawn2.getPieceName();
-        gameboard[7][2] = player2_Bishop1.getPieceName();
-        gameboard[6][2] = player2_pawn3.getPieceName();
-        gameboard[7][3] = player2_King.getPieceName();
-        gameboard[6][3] = player2_pawn4.getPieceName();
-        gameboard[7][4] = player2_Queen.getPieceName();
-        gameboard[6][4] = player2_pawn5.getPieceName();
-        gameboard[7][5] = player2_Bishop2.getPieceName();
-        gameboard[6][5] = player2_pawn6.getPieceName();
-        gameboard[7][6] = player2_Knight2.getPieceName();
-        gameboard[6][6] = player2_pawn7.getPieceName();
-        gameboard[7][7] = player2_Rook2.getPieceName();
-        gameboard[6][7] = player2_pawn8.getPieceName();
-
+        //set empty places
+        Piece emptyPlace;
         for (int i = 2; i < 6; i++) {
             for (int j = 0; j < 8; j++) {
                 if (gameboard[i][j] == null) {
-                    gameboard[i][j] = "(" + i + "," + j + ")";
+                    emptyPlace = new Piece(i, j);
+                    gameboard[i][j] = emptyPlace;
                 }
             }
         }
 
         //start the game
-        boolean king_alive =true;
-        while(king_alive){
+        boolean king_alive = true;
+        while (king_alive) {
             //show current board
-            for(int i=0; i<8; i++){
-                for(int j=0; j<8; j++){
-                    System.out.print(" | "+gameboard[i][j]);
+            for (int i = 0; i < 8; i++) {
+                System.out.print(8 - i);
+                for (int j = 0; j < 8; j++) {
+                    System.out.print(" | " + gameboard[i][j].getPieceName());
                 }
-                System.out.print(" |"+"\n");
+                System.out.print(" |" + "\n");
             }
+            System.out.println("     A    |    B    |    C    |    D    |    E    |    F    |     G    |    H");
+
+            //player1's time
             boolean player1_time = true;
-            while(player1_time) {
-                System.out.print(player1 + ", what do you want to move? Enter the piece's coordinate(ex. 2,4) ");
+            while (player1_time) {
+                System.out.print(player1 + ", what do you want to move? Enter the piece's coordinate(ex. 2,C) ");
                 String[] inputCo = input.nextLine().split(",");
-                int x = Integer.parseInt(inputCo[0]);
-                int y = Integer.parseInt(inputCo[1]);
-                if ((x<0 && x>7)||(y<0 && y>7)){
+                int x = 8-Integer.parseInt(inputCo[0]);
+                int y = changeYtoInt(inputCo[1]);
+                //if user input wrong coordinate, go back to while loop and ask again
+                if ((x < 0 && x > 7) || (y < 0 && y > 7)) {
                     System.out.println("You put the wrong coordinate. Try again.");
                     continue;
-                }else if(gameboard[x][y].equals(player1_pawn1.getPieceName())){
+                    //if Pawn is selected
+                } else if (gameboard[x][y].getPieceName().equals(player1_Pawn.getPieceName())) {
                     //ask user where to move the piece
+                    boolean cannot_move = true;
+                    while (cannot_move) {
+                        System.out.print(player1 + ", where do you want to move? Enter the coordinate(ex. 3,D) ");
+                        String[] destinationCo = input.nextLine().split(",");
+                        int destinationY = changeYtoInt(destinationCo[1]);
+                        gameboard = player1_Pawn.calPawnA(gameboard, x, y, 8-Integer.parseInt(destinationCo[0]), destinationY);
+                        if (gameboard[x][y].getPieceName().equals(player1_Pawn.getPieceName())) {
+                            System.out.println("Wrong destination. Try again.");
+                            continue;
+                        } else {
+                            cannot_move = false;
+                            player1_time = false;
+                        }
+                    }
+                    //if Rook is selected
+                }else if(gameboard[x][y].getPieceName().equals(player1_Rook.getPieceName())){
                     boolean cannot_move=true;
                     while(cannot_move) {
-                        System.out.print(player1 + ", where do you want to move? Enter the coordinate(ex. 3,5) ");
+                        System.out.print(player1 + ", where do you want to move? Enter the coordinate(ex. 3,D) ");
                         String[] destinationCo = input.nextLine().split(",");
-                        gameboard = player1_pawn1.calPawnA(gameboard, x, y, Integer.parseInt(destinationCo[0]), Integer.parseInt(destinationCo[1]));
-                        if(gameboard[x][y].equals(player1_pawn1.getPieceName())){
+                        int destinationY = changeYtoInt(destinationCo[1]);
+                        gameboard = player1_Rook.calRookA(gameboard, x, y, 8-Integer.parseInt(destinationCo[0]), destinationY);
+                        if(gameboard[x][y].getPieceName().equals(player1_Rook.getPieceName())){
                             System.out.println("Wrong destination. Try again.");
                             continue;
                         }else{
@@ -128,8 +117,50 @@ public class chessGame {
                         }
                     }
                 }
-            }
 
+            }
         }
+    }
+    public static String changeYtoString(int inputY){
+        if (inputY == 0){
+            AlphabetY = "A";
+        }else if(inputY == 1){
+            AlphabetY = "B";
+        }else if(inputY == 2){
+            AlphabetY = "C";
+        }else if(inputY == 3){
+            AlphabetY = "D";
+        }else if(inputY == 4){
+            AlphabetY = "E";
+        }else if(inputY == 5){
+            AlphabetY = "F";
+        }else if(inputY == 6){
+            AlphabetY = "G";
+        }else{
+            AlphabetY = "H";
+        }
+        return AlphabetY;
+    }
+    public static int changeYtoInt (String inputY){
+        if (inputY.equals("A")) {
+            integerY = 0;
+        } else if (inputY.equals("B")) {
+            integerY = 1;
+        } else if (inputY.equals("C")) {
+            integerY = 2;
+        } else if (inputY.equals("D")) {
+            integerY = 3;
+        } else if (inputY.equals("E")) {
+            integerY = 4;
+        } else if (inputY.equals("F")) {
+            integerY = 5;
+        } else if (inputY.equals("G")) {
+            integerY = 6;
+        } else if (inputY.equals("H")) {
+            integerY = 7;
+        } else {
+            integerY = -1;
+        }
+        return integerY;
     }
 }
