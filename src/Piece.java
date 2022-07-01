391ea034ca39ee3fce7d4d9f69fc24fcf846b45a
seungwoo99 <seung.code@gmail.com>
2022-07-01 23:39:30 +0900

    seventh commit - chess game 1. Add calculating Bishop, Queen, King, and Pawn movement method 2. Add player2 3. Add a method that knows when the game is over

--- src/Piece.java
+++ src/Piece.java
@@ -45,25 +45,69 @@ public class Piece {
         return empty;
     }
 
-    //calculate where can it move
-    //method that calculates pawn moves
+    //calculate where can the selected piece move
+    //method that calculates player2's Pawn move
     public Piece[][] calPawnA(Piece[][] board, int current_x, int current_y, int wanted_x, int wanted_y){
-        boolean check1= (wanted_x<7 && current_x+1 == wanted_x);
-        boolean check2=(current_y == wanted_y || current_y+1 == wanted_y || current_y-1 == wanted_y);
-        boolean check3=(current_y+1<8 && current_y-1>=0);
+        boolean check1 = (current_x< wanted_x && wanted_x < 8) && (0<= wanted_y && wanted_y <8);
 
-        if(check1&&check2){
-            if(wanted_y<8 && wanted_y >=0) {
-                board[wanted_x][wanted_y] = board[current_x][current_y];
-                board[current_x][current_y] = new Piece(current_x, current_y);
-                return board;
+        //initial move allows moving two blocks
+        if(check1){
+            //basic move
+            if(wanted_x - current_x == 1 && current_y == wanted_y){
+                //if opponent's piece is located on the wanted coordinate, then cannot move
+                if(board[wanted_x][wanted_y].getEmpty().equals("empty")){
+                    board[wanted_x][wanted_y] = board[current_x][current_y];
+                    board[current_x][current_y] = new Piece(current_x, current_y);
+                }
+                //initial two block move
+            }else if(current_x == 1 && wanted_x - current_x == 2 && current_y == wanted_y){
+                if(board[2][current_y].getEmpty().equals("empty") && board[3][current_y].getEmpty().equals("empty")){
+                    board[wanted_x][wanted_y] = board[current_x][current_y];
+                    board[current_x][current_y] = new Piece(current_x, current_y);
+                }
+                //take opponent's piece
+            }else if((current_x + 1 == wanted_x) && (current_y - 1 == wanted_y || current_y + 1 == wanted_y)){
+                if(!board[wanted_x][wanted_y].getPlayerName().equals(board[current_x][current_y].getPlayerName())){
+                    board[wanted_x][wanted_y] = board[current_x][current_y];
+                    board[current_x][current_y] = new Piece(current_x, current_y);
+                }
             }
-            return board;
-        }else {
-            return board;
         }
+
+        return board;
     }
-    //method that calculates Rook moves
+    //method that calculates player2's Pawn move
+    public Piece[][] calPawnB(Piece[][] board, int current_x, int current_y, int wanted_x, int wanted_y){
+        boolean check1 = (0<= wanted_x && wanted_x < current_x) && (0<= wanted_y && wanted_y <8);
+
+        //initial move allows moving two blocks
+        if(check1){
+            //basic move
+            if(current_x - wanted_x == 1 && current_y == wanted_y){
+                //if opponent's piece is located on the wanted coordinate, then cannot move
+                if(board[wanted_x][wanted_y].getEmpty().equals("empty")){
+                    board[wanted_x][wanted_y] = board[current_x][current_y];
+                    board[current_x][current_y] = new Piece(current_x, current_y);
+                }
+                //initial two block move
+            }else if(current_x == 6 && current_x - wanted_x == 2 && current_y == wanted_y){
+                if(board[5][current_y].getEmpty().equals("empty") && board[4][current_y].getEmpty().equals("empty")){
+                    board[wanted_x][wanted_y] = board[current_x][current_y];
+                    board[current_x][current_y] = new Piece(current_x, current_y);
+                }
+                //take opponent's piece
+            }else if((current_x - 1 == wanted_x) && (current_y - 1 == wanted_y || current_y + 1 == wanted_y)){
+                if(!board[wanted_x][wanted_y].getPlayerName().equals(board[current_x][current_y].getPlayerName())){
+                    board[wanted_x][wanted_y] = board[current_x][current_y];
+                    board[current_x][current_y] = new Piece(current_x, current_y);
+                }
+            }
+        }
+
+        return board;
+    }
+
+    //method that calculates Rook move
     public Piece[][] calRook(Piece[][] board, int current_x, int current_y, int wanted_x, int wanted_y){
         boolean check1 = (current_y == wanted_y)&&(wanted_x>=0 && wanted_x<8)&&(current_x!=wanted_x);
         boolean check2 = (current_x == wanted_x)&&(wanted_y>=0 && wanted_y<8)&&(current_y!=wanted_y);
@@ -131,7 +175,7 @@ public class Piece {
         return board;
     }
 
-    //method that calculates Knight moves
+    //method that calculates Knight move
     public Piece[][] calKnight(Piece[][] board, int current_x, int current_y, int wanted_x, int wanted_y){
         boolean check1 = (0<=wanted_x && wanted_x<8) && (0<=wanted_y && wanted_y<8);
         if(check1){
@@ -152,13 +196,74 @@ public class Piece {
         return board;
     }
 
-    //method that calculates Bishop moves
+    //method that calculates Bishop move
     public Piece[][] calBishop(Piece[][] board, int current_x, int current_y, int wanted_x, int wanted_y){
         boolean check1 = (0<=wanted_x && wanted_x<8) && (0<=wanted_y && wanted_y<8);
-        if(check1){
+        //difference between wanted and current value
+        int diffX = wanted_x - current_x;
+        int diffY = wanted_y - current_y;
+        boolean check2 = diffX == diffY || diffX == -diffY;
+        //if input destination value is appropriate, then move the Bishop
+        if(check1 && check2){
+            //check if other piece is located between them
+            somethingbetween = 0;
+            if(current_x < wanted_x && current_y < wanted_y){
+                for(int i=1; i<wanted_x-current_x; i++){
+                    if(board[current_x + i][current_y + i].getEmpty().equals("NotEmpty")){
+                        somethingbetween+=1;
+                    }
+                }
+            }else if(current_x < wanted_x && current_y > wanted_y){
+                for(int i=1; i<wanted_x-current_x; i++){
+                    if(board[current_x + i][current_y - i].getEmpty().equals("NotEmpty")){
+                        somethingbetween+=1;
+                    }
+                }
+            }else if(current_x > wanted_x && current_y > wanted_y){
+                for(int i=1; i<current_x - wanted_x; i++){
+                    if(board[current_x - i][current_y - i].getEmpty().equals("NotEmpty")){
+                        somethingbetween+=1;
+                    }
+                }
+            }else if(current_x > wanted_x && current_y < wanted_y){
+                for(int i=1; i<current_x - wanted_x; i++){
+                    if(board[current_x - i][current_y + i].getEmpty().equals("NotEmpty")){
+                        somethingbetween+=1;
+                    }
+                }
+            }
+            //if there is something between current coordinate and its destination
+            //then, do not move the piece
+            if(somethingbetween > 0) {
+                return board;
+            //if there is noting between current coordinate and its destination, and no player's piece on the destination coordinate
+                //then move the piece
+            }else if(somethingbetween==0 && ((board[wanted_x][wanted_y].getEmpty().equals("empty"))||
+                    !board[wanted_x][wanted_y].getPlayerName().equals(board[current_x][current_y].getPlayerName()))){
+                board[wanted_x][wanted_y] = board[current_x][current_y];
+                board[current_x][current_y] = new Piece(current_x, current_y);
+                return board;
+            }
 
         }
+        return board;
+    }
 
+    //method that calculates King move
+    public Piece[][] calKing(Piece[][] board, int current_x, int current_y, int wanted_x, int wanted_y){
+        boolean check1 = (0<=wanted_x && wanted_x<8) && (0<=wanted_y && wanted_y<8);
+        boolean check2 = (board[wanted_x][wanted_y].getEmpty().equals("empty"))||
+                !board[wanted_x][wanted_y].getPlayerName().equals(board[current_x][current_y].getPlayerName());
+        //difference between wanted and current value
+        int diffX = wanted_x - current_x;
+        int diffY = wanted_y - current_y;
+
+        if(check1 && check2){
+            if((-1 <= diffX && diffX <= 1)&&(-1 <= diffY && diffY <= 1)){
+                board[wanted_x][wanted_y] = board[current_x][current_y];
+                board[current_x][current_y] = new Piece(current_x, current_y);
+            }
+        }
         return board;
     }
 
--- src/chessGame.java
+++ src/chessGame.java
@@ -10,10 +10,16 @@ public class chessGame {
     public static void main(String[] args) {
         //ask users' name
         Scanner input = new Scanner(System.in);
-        System.out.print("What is your name? ");
+        System.out.print("What is your name?(Max Length: 7) ");
         String player1 = input.nextLine();
-        System.out.print("What is your opponent's name? ");
+        if(player1.length() > 7){
+            player1 = player1.substring(0,7);
+        }
+        System.out.print("What is your opponent's name?(Max Length: 7) ");
         String player2 = input.nextLine();
+        if(player2.length() > 7){
+            player2 = player2.substring(0,7);
+        }
 
         // set player1's pieces
         Piece player1_Rook = new Piece(player1, player1 + "_Rook");
@@ -62,19 +68,24 @@ public class chessGame {
         //start the game
         boolean king_alive = true;
         while (king_alive) {
+            //find the longest name between player1 and player2
+            int name_length = nameLength(player1, player2);
+
             //show current board
-            for (int i = 0; i < 8; i++) {
-                System.out.print(8 - i);
-                for (int j = 0; j < 8; j++) {
-                    System.out.print(" | " + gameboard[i][j].getPieceName());
-                }
-                System.out.print(" |" + "\n");
-            }
-            System.out.println("     A    |    B    |    C    |    D    |    E    |    F    |     G    |    H");
+            showBoard(gameboard, name_length);
 
             //player1's time
             boolean player1_time = true;
             while (player1_time) {
+                //check player1's king is alive
+                if(KingDead(player1, gameboard)){
+                    System.out.println(player1 + "'s king is dead. "+ player2 + " won!");
+                    king_alive = false;
+                    player1_time = false;
+                    continue;
+                }
+
+                //ask user player1 to select his piece
                 System.out.print(player1 + ", what do you want to move? Enter the piece's coordinate(ex. 2,C) ");
                 String[] inputCo = input.nextLine().split(",");
                 int x = 8-Integer.parseInt(inputCo[0]);
@@ -83,7 +94,8 @@ public class chessGame {
                 if ((x < 0 && x > 7) || (y < 0 && y > 7)) {
                     System.out.println("You put the wrong coordinate. Try again.");
                     continue;
-                    //if Pawn is selected
+
+                //if Pawn is selected
                 } else if (gameboard[x][y].getPieceName().equals(player1_Pawn.getPieceName())) {
                     //ask user where to move the piece
                     boolean cannot_move = true;
@@ -92,6 +104,7 @@ public class chessGame {
                         String[] destinationCo = input.nextLine().split(",");
                         int destinationY = changeYtoInt(destinationCo[1]);
                         gameboard = player1_Pawn.calPawnA(gameboard, x, y, 8-Integer.parseInt(destinationCo[0]), destinationY);
+                        //if the piece is not moved (The piece is not moved when the destination is wrong)
                         if (gameboard[x][y].getPieceName().equals(player1_Pawn.getPieceName())) {
                             System.out.println("Wrong destination. Try again.");
                             continue;
@@ -132,31 +145,208 @@ public class chessGame {
                             player1_time = false;
                         }
                     }
+                    //if Bishop is selected
+                }else if(gameboard[x][y].getPieceName().equals(player1_Bishop.getPieceName())){
+                    boolean cannot_move = true;
+                    while(cannot_move){
+                        System.out.print(player1 + ", where do you want to move? Enter the coordinate(ex. 3,D) ");
+                        String[] destinationCo = input.nextLine().split(",");
+                        int destinationY = changeYtoInt(destinationCo[1]);
+                        gameboard = player1_Rook.calBishop(gameboard, x, y, 8 - Integer.parseInt(destinationCo[0]), destinationY);
+                        if(gameboard[x][y].getPieceName().equals(player1_Bishop.getPieceName())){
+                            System.out.println("Wrong destination. Try again.");
+                            continue;
+                        }else{
+                            cannot_move = false;
+                            player1_time = false;
+                        }
+                    }
+                    //if Queen is selected
+                }else if(gameboard[x][y].getPieceName().equals(player1_Queen.getPieceName())){
+                    boolean cannot_move = true;
+                    while(cannot_move){
+                        System.out.print(player1 + ", where do you want to move? Enter the coordinate(ex. 3,D) ");
+                        String[] destinationCo = input.nextLine().split(",");
+                        int destinationY = changeYtoInt(destinationCo[1]);
+                        if(x == 8-Integer.parseInt(destinationCo[0]) || y == destinationY){
+                            gameboard = player1_Rook.calRook(gameboard, x, y, 8 - Integer.parseInt(destinationCo[0]), destinationY);
+                        }else{
+                            gameboard = player1_Rook.calBishop(gameboard, x, y, 8 - Integer.parseInt(destinationCo[0]), destinationY);
+                        }
+
+                        if(gameboard[x][y].getPieceName().equals(player1_Queen.getPieceName())){
+                            System.out.println("Wrong destination. Try again.");
+                            continue;
+                        }else{
+                            cannot_move = false;
+                            player1_time = false;
+                        }
+                    }
+                    //if King is selected
+                }else if(gameboard[x][y].getPieceName().equals(player1_King.getPieceName())){
+                    boolean cannot_move = true;
+                    while(cannot_move){
+                        System.out.print(player1 + ", where do you want to move? Enter the coordinate(ex. 3,D) ");
+                        String[] destinationCo = input.nextLine().split(",");
+                        int destinationY = changeYtoInt(destinationCo[1]);
+                        gameboard = player1_Rook.calKing(gameboard, x, y, 8 - Integer.parseInt(destinationCo[0]), destinationY);
+                        if(gameboard[x][y].getPieceName().equals(player1_King.getPieceName())){
+                            System.out.println("Wrong destination. Try again.");
+                            continue;
+                        }else{
+                            cannot_move = false;
+                            player1_time = false;
+                        }
+                    }
+                    //if nothing selected
+                }else{
+                    System.out.println("You chose the wrong one. Try again.");
+                }
+                //break line
+                System.out.println("");
+            }
+
+            //show Board
+            showBoard(gameboard, name_length);
+
+            //player2's time
+            boolean player2_time = true;
+            while(player2_time){
+                //check player1's king is alive
+                if(KingDead(player2, gameboard)){
+                    //announce that player2's king is dead
+                    System.out.println(player2 + "'s king is dead. "+ player1 + " won!");
+                    player2_time = false;
+                    king_alive = false;
+                    continue;
                 }
 
+                //ask user player2 to select his piece
+                System.out.print(player2 + ", what do you want to move? Enter the piece's coordinate(ex. 2,C) ");
+                String[] inputCo = input.nextLine().split(",");
+                int x = 8-Integer.parseInt(inputCo[0]);
+                int y = changeYtoInt(inputCo[1]);
+                //if user input wrong coordinate, go back to while loop and ask again
+                if ((x < 0 && x > 7) || (y < 0 && y > 7)) {
+                    System.out.println("You put the wrong coordinate. Try again.");
+                    continue;
+
+                    //if Pawn is selected
+                } else if (gameboard[x][y].getPieceName().equals(player2_Pawn.getPieceName())) {
+                    //ask user where to move the piece
+                    boolean cannot_move = true;
+                    while (cannot_move) {
+                        System.out.print(player2 + ", where do you want to move? Enter the coordinate(ex. 3,D) ");
+                        String[] destinationCo = input.nextLine().split(",");
+                        int destinationY = changeYtoInt(destinationCo[1]);
+                        gameboard = player2_Pawn.calPawnB(gameboard, x, y, 8-Integer.parseInt(destinationCo[0]), destinationY);
+                        //if the piece is not moved (The piece is not moved when the destination is wrong)
+                        if (gameboard[x][y].getPieceName().equals(player2_Pawn.getPieceName())) {
+                            System.out.println("Wrong destination. Try again.");
+                            continue;
+                        } else {
+                            cannot_move = false;
+                            player2_time = false;
+                        }
+                    }
+                    //if Rook is selected
+                }else if(gameboard[x][y].getPieceName().equals(player2_Rook.getPieceName())) {
+                    boolean cannot_move = true;
+                    while (cannot_move) {
+                        System.out.print(player2 + ", where do you want to move? Enter the coordinate(ex. 3,D) ");
+                        String[] destinationCo = input.nextLine().split(",");
+                        int destinationY = changeYtoInt(destinationCo[1]);
+                        gameboard = player2_Rook.calRook(gameboard, x, y, 8 - Integer.parseInt(destinationCo[0]), destinationY);
+                        if (gameboard[x][y].getPieceName().equals(player2_Rook.getPieceName())) {
+                            System.out.println("Wrong destination. Try again.");
+                            continue;
+                        } else {
+                            cannot_move = false;
+                            player2_time = false;
+                        }
+                    }
+                    //if Knight is selected
+                }else if(gameboard[x][y].getPieceName().equals(player2_Knight.getPieceName())){
+                    boolean cannot_move = true;
+                    while (cannot_move) {
+                        System.out.print(player2 + ", where do you want to move? Enter the coordinate(ex. 3,D) ");
+                        String[] destinationCo = input.nextLine().split(",");
+                        int destinationY = changeYtoInt(destinationCo[1]);
+                        gameboard = player2_Rook.calKnight(gameboard, x, y, 8 - Integer.parseInt(destinationCo[0]), destinationY);
+                        if (gameboard[x][y].getPieceName().equals(player2_Knight.getPieceName())) {
+                            System.out.println("Wrong destination. Try again.");
+                            continue;
+                        } else {
+                            cannot_move = false;
+                            player2_time = false;
+                        }
+                    }
+                    //if Bishop is selected
+                }else if(gameboard[x][y].getPieceName().equals(player2_Bishop.getPieceName())){
+                    boolean cannot_move = true;
+                    while(cannot_move){
+                        System.out.print(player2 + ", where do you want to move? Enter the coordinate(ex. 3,D) ");
+                        String[] destinationCo = input.nextLine().split(",");
+                        int destinationY = changeYtoInt(destinationCo[1]);
+                        gameboard = player2_Rook.calBishop(gameboard, x, y, 8 - Integer.parseInt(destinationCo[0]), destinationY);
+                        if(gameboard[x][y].getPieceName().equals(player2_Bishop.getPieceName())){
+                            System.out.println("Wrong destination. Try again.");
+                            continue;
+                        }else{
+                            cannot_move = false;
+                            player2_time = false;
+                        }
+                    }
+                    //if Queen is selected
+                }else if(gameboard[x][y].getPieceName().equals(player2_Queen.getPieceName())){
+                    boolean cannot_move = true;
+                    while(cannot_move){
+                        System.out.print(player2 + ", where do you want to move? Enter the coordinate(ex. 3,D) ");
+                        String[] destinationCo = input.nextLine().split(",");
+                        int destinationY = changeYtoInt(destinationCo[1]);
+                        if(x == 8-Integer.parseInt(destinationCo[0]) || y == destinationY){
+                            gameboard = player2_Rook.calRook(gameboard, x, y, 8 - Integer.parseInt(destinationCo[0]), destinationY);
+                        }else{
+                            gameboard = player2_Rook.calBishop(gameboard, x, y, 8 - Integer.parseInt(destinationCo[0]), destinationY);
+                        }
+
+                        if(gameboard[x][y].getPieceName().equals(player2_Queen.getPieceName())){
+                            System.out.println("Wrong destination. Try again.");
+                            continue;
+                        }else{
+                            cannot_move = false;
+                            player2_time = false;
+                        }
+                    }
+                    //if King is selected
+                }else if(gameboard[x][y].getPieceName().equals(player2_King.getPieceName())){
+                    boolean cannot_move = true;
+                    while(cannot_move){
+                        System.out.print(player2 + ", where do you want to move? Enter the coordinate(ex. 3,D) ");
+                        String[] destinationCo = input.nextLine().split(",");
+                        int destinationY = changeYtoInt(destinationCo[1]);
+                        gameboard = player2_Rook.calKing(gameboard, x, y, 8 - Integer.parseInt(destinationCo[0]), destinationY);
+                        if(gameboard[x][y].getPieceName().equals(player2_King.getPieceName())){
+                            System.out.println("Wrong destination. Try again.");
+                            continue;
+                        }else{
+                            cannot_move = false;
+                            player2_time = false;
+                        }
+                    }
+                    //if nothing selected
+                }else{
+                    System.out.println("You chose the wrong one. Try again.");
+                }
             }
+            //break line
+            System.out.println("");
         }
+        //announce that the game is over
+        System.out.println("Game Over");
     }
-    public static String changeYtoString(int inputY){
-        if (inputY == 0){
-            AlphabetY = "A";
-        }else if(inputY == 1){
-            AlphabetY = "B";
-        }else if(inputY == 2){
-            AlphabetY = "C";
-        }else if(inputY == 3){
-            AlphabetY = "D";
-        }else if(inputY == 4){
-            AlphabetY = "E";
-        }else if(inputY == 5){
-            AlphabetY = "F";
-        }else if(inputY == 6){
-            AlphabetY = "G";
-        }else{
-            AlphabetY = "H";
-        }
-        return AlphabetY;
-    }
+
+    //method that change alphabet to integer
     public static int changeYtoInt (String inputY){
         if (inputY.equals("A")) {
             integerY = 0;
@@ -178,5 +368,52 @@ public class chessGame {
             integerY = -1;
         }
         return integerY;
+    }
+
+    //find the longest name to make all blocks' size equal
+    public static int nameLength(String player1_Name, String player2_Name){
+        int longestName;
+        if(player1_Name.length() > player2_Name.length()){
+            longestName = player1_Name.length();
+        }else{
+            longestName = player2_Name.length();
+        }
+        return longestName+7;
+    }
+
+    //method that shows the current board
+    public static void showBoard(Piece[][] board, int blockLength){
+        //show current board
+        System.out.println(" ".repeat((blockLength*8-10)/2)+"<Chess Board>");
+        for (int i = 0; i < 8; i++) {
+            System.out.print(8 - i + " ");
+            for (int j = 0; j < 8; j++) {
+                int blank = blockLength - board[i][j].getPieceName().length();
+                System.out.print("|" + " ".repeat(blank/2)+ board[i][j].getPieceName()
+                                     +" ".repeat(blank-blank/2));
+            }
+            System.out.print("|" + "\n");
+        }
+        System.out.println("  |"+" ".repeat(blockLength/2)+"A"+" ".repeat(blockLength-blockLength/2 -1)+"|"
+                +" ".repeat(blockLength/2)+"B"+" ".repeat(blockLength-blockLength/2 -1)+"|"
+                +" ".repeat(blockLength/2)+"C"+" ".repeat(blockLength-blockLength/2 -1)+"|"
+                +" ".repeat(blockLength/2)+"D"+" ".repeat(blockLength-blockLength/2 -1)+"|"
+                +" ".repeat(blockLength/2)+"E"+" ".repeat(blockLength-blockLength/2 -1)+"|"
+                +" ".repeat(blockLength/2)+"F"+" ".repeat(blockLength-blockLength/2 -1)+"|"
+                +" ".repeat(blockLength/2)+"G"+" ".repeat(blockLength-blockLength/2 -1)+"|"
+                +" ".repeat(blockLength/2)+"H"+" ".repeat(blockLength-blockLength/2 -1)+"|");
+    }
+
+    //method that checks if the king is dead
+    public static boolean KingDead(String playername, Piece[][] board){
+        boolean king_Dead = true;
+        for(int row=0; row<8; row++){
+            for(int column=0; column<8; column++){
+                if(board[row][column].getPieceName().equals(playername + "_King")){
+                    king_Dead = false;
+                }
+            }
+        }
+        return king_Dead;
     }
 }
